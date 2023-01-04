8ed44d5982c9fe23ac574883c596480843b9035f
Sergio Penagos <60018479+Semi-dios@users.noreply.github.com>
2023-01-04 11:11:21 -0500

    feat: Added components' funcionalities   (#5)
    * feat-workflows
    
    * feat-workflows
    
    * feat-i18n
    
    * feat-i18n
    
    * feat-css
    
    * feat-funcionalities

---
+++ src/components/Tile.tsx
@@ -0,0 +1,43 @@
+import { isTemplateExpression } from 'typescript';
+import { getSunTime } from '../helpers';
+import { forecastType } from '../types';
+import Feels from './Icons/Feels';
+import Humidity from './Icons/Humidity';
+import Pop from './Icons/Pop';
+import Pressure from './Icons/Pressure';
+import Visibility from './Icons/Visibility';
+import Wind from './Icons/Wind';
+
+type Props = {
+    icon: 'wind' | 'feels' | 'humidity' | 'visibility' | 'pressure' | 'pop';
+    title: string;
+    info: string | JSX.Element;
+    description: string;
+};
+
+const icons = {
+    wind: Wind,
+    feels: Feels,
+    humidity: Humidity,
+    visibility: Visibility,
+    pressure: Pressure,
+    pop: Pop,
+};
+
+const Tile = ({ icon, title, info, description }: Props): JSX.Element => {
+    const Icon = icons[icon];
+    return (
+        <article
+            className=" w-[140px] h-[130px] text-zinc-700  flex flex-col justify-between
+         bg-white/20  backdrop-blur-lg rounded drop-shadow-lg p-2 mb-5"
+        >
+            <div className="flex items-center text-sm font-bold">
+                <Icon /> <h4 className="ml-1">{title}</h4>
+            </div>
+            <h3 className="mt-2 text-lg">{info}</h3>
+            <p className="text-xs font-bold">{description}</p>
+        </article>
+    );
+};
+
+export default Tile;
